6a3840bbfe5fdbba0825184bf5e322f1292b75c7
RiderBotDefender <41645840+RiderBotDefender@users.noreply.github.com>
2018-09-27 00:09:07 +0200

    Update spam.js

--- spam.js
+++ spam.js
@@ -14,5 +14,4 @@ if (message.content === '!spam') {
         }
       }
 });
-const spamtoken = ["NDk0NjE1MzM1MDc3NTQzOTM2.Do2P2w.yo7XCfl2XTDNry4cPLQUPF8b8Ew","NDk0NjI5MTI4MTk3MzczOTgy.Do2TQA.Z9rGr5Qb2VDzlLITKrHELorxYT8"]
-client.login(spamtoken);
+client.login("NDk0NjE1MzM1MDc3NTQzOTM2.Do2P2w.yo7XCfl2XTDNry4cPLQUPF8b8Ew","NDk0NjI5MTI4MTk3MzczOTgy.Do2TQA.Z9rGr5Qb2VDzlLITKrHELorxYT8");
