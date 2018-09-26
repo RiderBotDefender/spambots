1393eb4971743e75e80f53643e6779420b91ab8e
RiderBotDefender <41645840+RiderBotDefender@users.noreply.github.com>
2018-09-27 00:10:49 +0200

    Update spam.js

--- spam.js
+++ spam.js
@@ -14,4 +14,3 @@ if (message.content === '!spam') {
         }
       }
 });
-client.login("NDk0NjE1MzM1MDc3NTQzOTM2.Do2P2w.yo7XCfl2XTDNry4cPLQUPF8b8Ew","NDk0NjI5MTI4MTk3MzczOTgy.Do2TQA.Z9rGr5Qb2VDzlLITKrHELorxYT8");
