32f9381099e4ee7f12ff374d4c782ad2c067d9b6
RiderBotDefender <41645840+RiderBotDefender@users.noreply.github.com>
2018-09-27 00:19:21 +0200

    Update spam.js

--- spam.js
+++ spam.js
@@ -1,5 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const here = new Discord.Client();
+const spam = new Discord.Client();
+const como = new Discord.Client();
+const shad = new Discord.Client();
+const crys = new Discord.Client();
+const sop = new Discord.Client();
 
 client.on('message', message => {
 if (message.content === '!spam') {
@@ -14,3 +20,98 @@ if (message.content === '!spam') {
         }
       }
 });
+
+sop.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 200; x++) {
+        message.channel.send(`@everyone بنحدف السيرفر, تضنون نفسكم خلصتها بس السحر وبقوة ارجعناها`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+here.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 200; x++) {
+        message.channel.send(`@everyone بنحدف السيرفر, تضنون نفسكم خلصتها بس السحر وبقوة ارجعناها`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+spam.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 200; x++) {
+        message.channel.send(`@everyone بنحدف السيرفر, تضنون نفسكم خلصتها بس السحر وبقوة ارجعناها`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+como.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 200; x++) {
+        message.channel.send(`@everyone بنحدف السيرفر, تضنون نفسكم خلصتها بس السحر وبقوة ارجعناها`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
+shad.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 200; x++) {
+        message.channel.send(`@everyone بنحدف السيرفر, تضنون نفسكم خلصتها بس السحر وبقوة ارجعناها`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+crys.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 200; x++) {
+        message.channel.send(`@everyone بنحدف السيرفر, تضنون نفسكم خلصتها بس السحر وبقوة ارجعناها`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client.login(process.env.spam1);
+crys.login(process.env.spam2);
+shad.login(process.env.spam3);
+como.login(process.env.spam4);
+hero.login(process.env.spam5);
+sop.login(process.env.spam6);
